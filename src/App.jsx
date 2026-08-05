579e9fbac365d1c70ae63789021b9fc9d7841d84
BrunoOlivera123 <beenicontact@gmail.com>
2026-08-05 20:39:39 -0300

    At0508

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import './App.css'
 function App() {
   return (
     <div>
-        <h1> Home </h1>
+        <h1> Vite Exemplo </h1>
+        <h2> Exemplo H2</h2>
+        <h3> Exemplo H3</h3>
+        <p>Exemplo parágrafo</p>
     </div>
   )
 }
